af58318396a31c903d621cacd488bd23edc259dc
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-11-25 03:29:00 +0600

    users collection api created

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+//middleware
+
+app.use(cors())
+app.use(express.json())
+
+const uri = "mongodb+srv://VelocitiWorkUser:2D4A7e2SDxPhxkfM@cluster0.opj08k2.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const usersCollection = client.db('VelocitiWork').collection('users')
+
+
+    app.post('/users', async(req, res)=>{
+        const users = req.body;
+        const result = await usersCollection.insertOne(users)
+        res.send(result)
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello VelocitiWork-Ventures!')
