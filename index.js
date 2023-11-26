8686a162f5b238405badffe7d589b9152bbc8993
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-11-27 03:04:04 +0600

    payment method api done

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 //middleware
 
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://VelocitiWorkUser:2D4A7e2SDxPhxkfM@cluster0.opj08k2.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.opj08k2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,15 +31,49 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const usersCollection = client.db('VelocitiWork').collection('users')
+    const paymentCollection = client.db('VelocitiWork').collection('payments')
 
 
-    app.post('/users', async(req, res)=>{
-        const users = req.body;
-        const result = await usersCollection.insertOne(users)
-        res.send(result)
+    //payment APIs
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const { salary } = req.body;
+      const amount = parseInt(salary * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
-    app.get('/users', async(req, res)=>{
+
+    app.post('/payments', async (req, res) => {
+      const payments = req.body;
+      const result = await paymentCollection.insertOne(payments)
+      res.send(result)
+
+    })
+
+    app.get('/payments', async (req, res) => {
+      const result = await paymentCollection.find().toArray()
+      res.send(result)
+    })
+
+    //apis
+
+
+    app.post('/users', async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users)
+      res.send(result)
+    })
+
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
@@ -47,13 +82,17 @@ async function run() {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
-          $set: {
-              verified: 'yes'
-          }
+        $set: {
+          verified: 'yes'
+        }
       }
       const result = await usersCollection.updateOne(filter, updatedDoc)
       res.send(result)
-  })
+    })
+
+    
+
+
 
 
   } finally {
